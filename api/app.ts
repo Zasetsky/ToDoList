2a2f4c89182c8ca7787f53ed97480ca456279f81
Mikhail Podolskiy <Edge769@yandex.ru>
2022-01-13 00:34:39 +0300

    add api implementation

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,62 @@
 import express from 'express';
+import { Task } from './models';
 
+let tasks: Task[] = [];
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
+app
+  .get('/tasks', (req, res) => {
+    res.send(tasks);
+  })
+  .post('/tasks', (req, res) => {
+    const { description } = req.body as Task;
+    if (!description) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const id = tasks.reduce((res, curr) => {
+      return res > curr.id ? res : curr.id;;
+    }, 0);
+
+    tasks.push({ description, isCompleted: false, id });
+    res.sendStatus(204);
+  })
+  .get('/tasks/:id', (req, res) => {
+    const id = parseFloat(req.params['id']);
+    const task = tasks.find(t => t.id == id);
+    if (!task) {
+      res.sendStatus(404);
+    } else {
+      res.send(task);
+    }
+  })
+  .delete('/tasks/:id', (req, res) => {
+    const id = parseFloat(req.params['id']);
+    const task = tasks.find(t => t.id == id);
+    if (task) {
+      tasks = tasks.filter(t => t.id != id);
+    } 
+    
+    res.sendStatus(204);
+  })
+  .put('/tasks/:id', (req, res) => {
+    const id = parseFloat(req.params['id']);
+    const task = tasks.find(t => t.id == id);
+    if (task) {
+      const { description, isCompleted } = req.body as Task;
+      if (!description) {
+        res.sendStatus(400);
+        return;
+      }
+
+      task.description = description;
+      task.isCompleted = isCompleted;
+      res.sendStatus(202);
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
 app.listen(port, ((err: any) => {
   if (err) {
